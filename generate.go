603c55019d40d748e023aae23a445838b3701652
romanidzs <romanidzs@gmail.com>
2022-10-11 20:32:18 +0000

    fix

--- generate.go
+++ generate.go
@@ -4,4 +4,4 @@ package sharebusiness
 //go:generate swagger generate server -t ./transport/rest/restapi -f ./swagger.yaml --exclude-main
 //go:generate swagger generate client -t ./transport/rest/restapi -f ./swagger.yaml
 
-//go:generate protoc --go_out=./transport/grpc --go_opt=paths=source_relative --go-grpc_out=./transport/grpc --go-grpc_opt=paths=source_relative ./grpc.proto
+//go:generate protoc --go_out=./transport/grpc --go_opt=paths=source_relative --go-grpc_out=./transport/grpc --go-grpc_opt=paths=source_relative ./transport/grpc.proto
