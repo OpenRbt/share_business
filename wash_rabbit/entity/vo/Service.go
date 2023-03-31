eafbe3a9010f1417f99273f33e094923c4353eee
Kronusol <kronusol@yandex.ru>
2023-03-31 19:17:12 +0700

    Fixed rabbit service vo

--- wash_rabbit/entity/vo/Service.go
+++ wash_rabbit/entity/vo/Service.go
@@ -3,6 +3,6 @@ package vo
 type Service string
 
 const (
-	WashAdminService string = "wash_admin_service"
-	WashBonusService string = "wash_bonus_service"
+	WashAdminService Service = "wash_admin_service"
+	WashBonusService         = "wash_bonus_service"
 )
